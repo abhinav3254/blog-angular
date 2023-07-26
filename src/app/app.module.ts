ec70e1207bfc82f6c23a2321a32771df3b87b477
abhinav3254 <abhinav3254@gmail.com>
2023-07-26 21:45:55 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BlogContentComponent } from './blog-content/blog-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavBarComponent
+    NavBarComponent,
+    BlogContentComponent
   ],
   imports: [
     BrowserModule,
