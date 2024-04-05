ef85a32009e7ad535673495eba754c5096f2225f
abhinav3254 <abhinav3254@gmail.com>
2024-04-05 22:21:18 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,8 +7,8 @@ import { BlogModel } from 'src/interfaces/Blog';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
-  blogs!:BlogModel[]; 
+
+  blogs!: BlogModel[];
 
   ngOnInit(): void {
     this.blogs = blogsJson;
@@ -18,32 +18,32 @@ export class HomeComponent implements OnInit {
 
 const blogsJson = [
   {
-  "id":1,
-  "title":"I sold my startup because of bugs: I wish I had this serverless repository!",
-  "content":"This is a very naive solution that was running on multiple EC2 (AWS) and Redis.In theory, it should work - Yes, you would need to scale the workers horizontally, but that was not my problem. I started to experience something else - memory leaks.As you probably know, Puppeteer - a chromium automated browser, is heavy (same as Chrome on your computer), and I was running lots of them - they sucked both my memory - causing the servers to stop working and took the internet bandwidth.",
-  "image":"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fikv2vuehxrwp6k8nhyl3.png",
-  "username":"Ranjan Dailata",
-  "publishDate":"13 sept 2023",
-  "tags":[
+    "id": 1,
+    "title": "I sold my startup because of bugs: I wish I had this serverless repository!",
+    "content": "This is a very naive solution that was running on multiple EC2 (AWS) and Redis.In theory, it should work - Yes, you would need to scale the workers horizontally, but that was not my problem. I started to experience something else - memory leaks.As you probably know, Puppeteer - a chromium automated browser, is heavy (same as Chrome on your computer), and I was running lots of them - they sucked both my memory - causing the servers to stop working and took the internet bandwidth.",
+    "image": "https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fikv2vuehxrwp6k8nhyl3.png",
+    "username": "Ranjan Dailata",
+    "publishDate": "13 sept 2023",
+    "tags": [
       "webdev",
       "javascript",
       "programming",
       "opensource"
-      ]
+    ]
   }
   ,
   {
-      "id":1,
-      "title":"I sold my startup because of bugs: I wish I had this serverless repository!",
-      "content":"This is a very naive solution that was running on multiple EC2 (AWS) and Redis.In theory, it should work - Yes, you would need to scale the workers horizontally, but that was not my problem. I started to experience something else - memory leaks.As you probably know, Puppeteer - a chromium automated browser, is heavy (same as Chrome on your computer), and I was running lots of them - they sucked both my memory - causing the servers to stop working and took the internet bandwidth.",
-      "image":"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fikv2vuehxrwp6k8nhyl3.png",
-      "username":"Ranjan Dailata",
-      "publishDate":"13 sept 2023",
-      "tags":[
-          "webdev",
-          "javascript",
-          "programming",
-          "opensource"
-          ]
-      }
+    "id": 2,
+    "title": "I sold my startup because of bugs: I wish I had this serverless repository!",
+    "content": "This is a very naive solution that was running on multiple EC2 (AWS) and Redis.In theory, it should work - Yes, you would need to scale the workers horizontally, but that was not my problem. I started to experience something else - memory leaks.As you probably know, Puppeteer - a chromium automated browser, is heavy (same as Chrome on your computer), and I was running lots of them - they sucked both my memory - causing the servers to stop working and took the internet bandwidth.",
+    "image": "https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fikv2vuehxrwp6k8nhyl3.png",
+    "username": "Ranjan Dailata",
+    "publishDate": "13 sept 2023",
+    "tags": [
+      "webdev",
+      "javascript",
+      "programming",
+      "opensource"
+    ]
+  }
 ];
